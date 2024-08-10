a2a8b1b54c0fc6409b273449852dcf6794825060
Cristofer <cristoferportela@gmail.com>
2024-08-10 20:14:16 -0300

    fazendo chamada para a home

--- src/main/kotlin/com/qk/quark/MainEntrance.kt
+++ src/main/kotlin/com/qk/quark/MainEntrance.kt
@@ -1,30 +1,13 @@
 package com.qk.quark
 
+import com.qk.views.Home
 import javafx.application.Application
-import javafx.fxml.FXMLLoader
-import javafx.scene.Scene
-import javafx.stage.Screen
 import javafx.stage.Stage
 
 class MainEntrance : Application() {
     override fun start(stage: Stage) {
-        val res = MainEntrance::class.java
-
-        val fxmlLoader = FXMLLoader(res.getResource("home.fxml"))
-
-        val screenBounds = Screen.getPrimary().visualBounds
-
-        val scene = Scene(fxmlLoader.load(), screenBounds.width, screenBounds.height - 35)
-        val css = res.getResource("/styles.css")?.toExternalForm()
-
-        if (css != null) {
-            scene.stylesheets.add(css)
-        } else {
-            println("Arquivo CSS não encontrado")
-        }
-        stage.title = "Hello!"
-        stage.scene = scene
-        stage.show()
+        val home = Home(stage)
+        home.indexHome()
     }
 }
 
