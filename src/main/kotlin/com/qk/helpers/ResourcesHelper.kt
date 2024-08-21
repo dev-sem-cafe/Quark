8e31abf58f6b36279425304b851f946c82af4531
Cristofer <cristoferportela@gmail.com>
2024-08-21 20:38:32 -0300

    feat: Routes and Style classes added

--- src/main/kotlin/com/qk/helpers/ResourcesHelper.kt
+++ src/main/kotlin/com/qk/helpers/ResourcesHelper.kt
@@ -1,4 +1,25 @@
 package com.qk.helpers
 
-class ResourcesHelper {
+import com.qk.quark.Quark
+import javafx.geometry.Rectangle2D
+import javafx.stage.Screen
+import java.net.URL
+
+fun getRes(res: String): URL? {
+    return Quark::class.java.getResource(res)
+}
+fun size(): Rectangle2D {
+    return Screen.getPrimary().visualBounds
+}
+
+class Routes {
+    companion object {
+        const val home = "home.fxml"
+    }
+}
+
+class Style {
+    companion object {
+        const val mainStyle = "styles.css"
+    }
 }
